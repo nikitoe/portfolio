35cb04b519c87179791ea8f5bafc9c4423cdaf06
Jiyong Sim <sjy19910222@gmail.com>
2022-02-03 15:38:01 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -4,8 +4,6 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight :${navbarHeight}`);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     }else {
@@ -27,11 +25,20 @@ navbarMenu.addEventListener('click', (event) => {
 
 // Handle click on "contact me" button on home
 // Home화면의 contact me 버튼 클릭시 하단의 contact화면으로 스크롤링 된게 만든다.
-
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
+
+// Make home slowly fade to transparent as the window scrolls down
+// 스크롤을 아래로 내리면 홈화면의 투명도가 변하게 만든다.
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity =  1 - window.scrollY / homeHeight;
+});
+
+
 
 function scrollIntoView (selector) {
     const scrollTo = document.querySelector(selector);
