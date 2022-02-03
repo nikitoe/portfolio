d8de0158adf793f956a83621684e97c5f8bb2318
Jiyong Sim <sjy19910222@gmail.com>
2022-02-03 13:52:41 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -11,4 +11,18 @@ document.addEventListener('scroll', () => {
     }else {
         navbar.classList.remove('navbar--dark');
     }
-})
+});
+
+// navbar 메뉴를 눌렀을때 스크롤링 되게 만든다.
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
